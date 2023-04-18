34ad98f426f3adf5853c83f579ffc63a323228fd
Caleb Thompson <caleb.softdev@gmail.com>
2023-04-18 04:25:52 -0700

    mongo db uri

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = process.env.MONGOD;
+const luri =
+  "mongodb+srv://calebson:MrgoBNuYzOhucxlX@cluster0.awnhm16.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(cors());
 
 // connect to MongoDB using Mongoose
 mongoose
-  .connect(uri, { useNewUrlParser: true })
+  .connect(luri, { useNewUrlParser: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
